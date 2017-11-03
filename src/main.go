e17e5870593cf6236de7614e1099e1e939abb216
benBen <benmbenjamin@gmail.com>
2017-11-02 19:37:20 -0700

    broadcast upon message receipt (per scotch)

--- src/main.go
+++ src/main.go
@@ -3,35 +3,73 @@ package main
 import (
   "net/http"
   "log"
-  "fmt"
   "github.com/gorilla/websocket"
 )
 
+var clients = make(map[*websocket.Conn]bool) // connected clients
+var broadcast = make(chan Message) // broadcast channel
 var upgrader = websocket.Upgrader{}
 
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "index.html")
-  })
+  fs := http.FileServer(http.Dir("../public"))
+  http.Handle("/", fs)
 
-  http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-    conn, err := upgrader.Upgrade(w, r, nil)
+  http.HandleFunc("/ws", handleConnections)
+
+  // Start listening for incoming messages
+  go handleMessages()
+
+  log.Println("Party on :7000...")
+  err := http.ListenAndServe(":7000", nil)
+  if err != nil {
+    log.Fatal("ListenAndServe: ", err)
+  }
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+  // Upgrade initial GET to a websocket
+  conn, err := upgrader.Upgrade(w, r, nil)
+  if err != nil {
+    log.Fatal(err)
+    return
+  }
+
+  clients[conn] = true
+
+  for {
+    var msg Message
+
+    err = conn.ReadJSON(&msg)
     if err != nil {
-      log.Println(err)
-      return
+      log.Printf("fcukin error: %v", err)
+      break
     }
 
-    for {
-      _, p, err := conn.ReadMessage()
+    broadcast <- msg
+  }
+
+  // Close connection when function returns
+  defer conn.Close()
+}
+
+func handleMessages() {
+  for {
+    // Grab next message from broadcast channel and...
+    msg := <- broadcast
+    // ...send it to every connected client
+    for client := range clients {
+      log.Printf("received: %s", msg)
+
+      err := client.WriteJSON(msg)
       if err != nil {
-        return
+        log.Printf("error: %v", err)
+        client.Close()
+        delete(clients, client)
       }
-
-      msg := string(p)
-
-      fmt.Printf("received: %s\n", msg)
     }
-  })
+  }
+}
 
-  http.ListenAndServe(":7000", nil)
+type Message struct {
+  Type string `json:"type"`
 }
