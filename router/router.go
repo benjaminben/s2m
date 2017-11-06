d726e3136def6e8d36cc8df4021362fa7719b1c6
benBen <benmbenjamin@gmail.com>
2017-11-05 17:07:26 -0800

    index tmpl vs web

--- router/router.go
+++ router/router.go
@@ -12,13 +12,22 @@ func Make() *httprouter.Router {
   Router := httprouter.New()
 
   Router.GET("/ws", SocketHandler)
-  Router.GET("/", defaultHandler)
+  Router.GET("/", indexHandler)
+  Router.GET("/room/:id", webHandler)
 
   return Router
 }
 
-func defaultHandler(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-  room := models.Room{RoomID: "BLuuboobs"}
+func indexHandler(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+  t := template.Must(template.ParseFiles("tmpl/index.html"))
+  err := t.ExecuteTemplate(res, "index", nil)
+  if err != nil {
+    panic(err)
+  }
+}
+
+func webHandler(res http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+  room := models.Room{RoomID: ps.ByName("id")}
   t := template.Must(template.ParseFiles("tmpl/web.html"))
   err := t.ExecuteTemplate(res, "web", room)
   if err != nil {
