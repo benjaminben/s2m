25b859cc51dfd806f1b280379d8de7699b610118
benBen <benmbenjamin@gmail.com>
2017-11-06 17:57:38 -0800

    add canvas client and Messages.coords field

--- models/message.go
+++ models/message.go
@@ -1,6 +1,7 @@
 package models
 
 type Message struct {
-  Type      string `json:"type"`
-  Timestamp string `json:"timestamp"`
+  Type      string    `json:"type"`
+  Timestamp string    `json:"timestamp"`
+  Coords    []float64 `json:"coords"`
 }
